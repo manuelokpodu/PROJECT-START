e7bf3578914114e79d6e9c4e0a4e81877c4ff75d
Okpodu Emmanuel <manuelokpodu@gmail.com>
2025-01-09 15:14:29 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>This is the starting</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque
+        laudantium suscipit quos aperiam, odit asperiores officiis sequi qui est
+        voluptates impedit. Mollitia, dolores praesentium aspernatur nam
+        quibusdam dolorum asperiores voluptatum.
+      </p>
+    </>
+  );
+}
+
+export default App;
